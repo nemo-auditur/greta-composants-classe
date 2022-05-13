1ce141513f9aba6a563cf1a52ee4e29b7db7f8ea
Clément Legal <clement@macbook-pro-de-clement.home>
2022-05-13 16:36:06 +0200

    add remove student of the class

--- src/App.js
+++ src/App.js
@@ -31,74 +31,91 @@ function App() {
       Ma classe:
       <div className="containerClasse">
         {
-        // On boucle sur le state classe pour récupérer tous les élèves 
-        // grâce à un map
-        classe.map((eleve, index) => {
-          // je retourne les élève un à un
-          return (
-            <div key={index} className="containerEleve">
-              <div
-                onClick={()=>{
-                  const classCopy = [...classe]
-                  classCopy.splice(index,1)
-                  setClasse(classCopy)
-                }}
-              >x</div>
-              Élève :
-              <Prenom propsFirstname={eleve.firstname} />
-              <Nom propsName={eleve.name} />
-            </div>
-          );
-        })}
+          // On boucle sur le state classe pour récupérer tous les élèves
+          // grâce à un map
+          // En plus, on ajout le paramètre index, afin d'avoir
+          // l'index de chaque élève dans le tableau. 
+          classe.map((eleve, index) => {
+            // je retourne les élève un à un
+            return (
+              <div key={index} className="containerEleve">
+                <div
+                  className="eraserCross"
+                  onClick={()=>{
+                    // 1- On ne peut pas modifier le state directement
+                    // Il faut donc obligatoirement en faire une copie
+                    const classeCopy = [...classe]
+                    // 2- On opère notre modification sur la copie:
+                    // en l'occurence, on enlève l'élève voulu
+                    classeCopy.splice(index,1)
+                    // 3- On met à jour notre state classe avec la copie
+                    // modifiée
+                    setClasse(classeCopy)
+                  }}
+                >
+                  x
+                </div>
+                Élève :
+                <Prenom propsFirstname={eleve.firstname} />
+                <Nom propsName={eleve.name} />
+              </div>
+            );
+          })
+        }
       </div>
-      <div>
-        Création nouvel élève:
-        <div className="containerEleve">
-          <div className="styleFirstName">
-            <label>Prénom</label>
+      Outil de création d'un nouvel élève:
+      <div className="containerEleve">
+        <div className="styleFirstNameInput">
+          <div className="inputContainer">
+            <label className="inputLabel">Prénom</label>
             <input
-              placeholder="entrez un prénom"
+              type="text"
+              placeholder="Entrez un prénom"
               value={newStudentFirstname}
               onChange={(event) => {
-                // je modifie newStudentFirstname grâce à mon setNewStudentFirstname 
+                // je modifie newStudentFirstname grâce à mon setNewStudentFirstname
                 setNewStudentFirstname(event.target.value);
               }}
             />
-            <div>{newStudentFirstname}</div>
           </div>
-          <div className="styleName">
-            <label>Nom</label>
-            <input
-              placeholder="entrez un nom"
-              value={newStudentName}
-              onChange={(event) => {
-                // je modifie newStudentName grâce à mon setNewStudentName 
-                setNewStudentName(event.target.value);
-              }}
-            />
-            <div>{newStudentName}</div>
-          </div>
-          <button
-            onClick={() => {
-              //1 - On fait une VRAIE copie de classe
-              const classCopy = [...classe];
-              //2 - On ajoute un élèvement à classe
-              // grâce aux states newStudentName & newStudentFirstname
-              classCopy.push({
-                name: newStudentName,
-                firstname: newStudentFirstname,
-              });
-              //3 - Je mets à jour ma classe avec la copie qui contient
-              //le nouvel élève
-              setClasse(classCopy);
-              //4- Je remets à 0 newStudentFirstname & newStudentName
-              setNewStudentFirstname("");
-              setNewStudentName("");
-            }}
-          >
-            Ajouter un nouvel élève
-          </button>
+          <div>{newStudentFirstname}</div>
         </div>
+        <div className="styleNameInput">
+          <div className="inputContainer">
+          <label className="inputLabel">Nom</label>
+          <input
+            type="text"
+            placeholder="Entrez un nom"
+            value={newStudentName}
+            onChange={(event) => {
+              // je modifie newStudentName grâce à mon setNewStudentName
+              setNewStudentName(event.target.value);
+            }}
+          />
+          </div>
+          <div>{newStudentName}</div>
+        </div>
+        <button
+          onClick={() => {
+            //1 - On fait une VRAIE copie de classe
+            //car on ne peut pas modifier un state directement
+            const classCopy = [...classe];
+            //2 - On ajoute un élèvement à classe
+            // grâce aux states newStudentName & newStudentFirstname
+            classCopy.push({
+              name: newStudentName,
+              firstname: newStudentFirstname,
+            });
+            //3 - Je mets à jour mon state classe avec la copie qui contient
+            //le nouvel élève
+            setClasse(classCopy);
+            //4- Je remets à 0 newStudentFirstname & newStudentName
+            setNewStudentFirstname("");
+            setNewStudentName("");
+          }}
+        >
+          Ajouter un nouvel élève
+        </button>
       </div>
     </div>
   );
